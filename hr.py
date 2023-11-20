8d357157e84e2bf0ca951ceff2f10c22fdf16761
Techie <149437995+FarRza111@users.noreply.github.com>
2023-11-20 09:05:54 +0200

    Update hr.py

--- hr.py
+++ hr.py
@@ -1,3 +1,6 @@
+from abc import  ABC,abstractmethod
+
+
 class PayrollSystem:
 
     def calculate_payroll(self, employees):
@@ -9,41 +12,6 @@ class PayrollSystem:
             print(f"-Chech Amount: {employee.calculate_payroll()}")
 
 
-class Employee:
-
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
-class SalaryEmployee(Employee):
-
-    def __init__(self, id, name, weekly_salary):
-        super().__init__(id, name)
-        self.weekly_salary =  weekly_salary
-
-    def calculate_payroll(self):
-        return self.weekly_salary
-
-class HourlyEmployee(Employee):
-  
-    def __init__(self, id, name, hours_worked, hour_rate):
-        super().__init__(id, name)
-        self.hours_worked = hours_worked
-        self.hour_rate = hour_rate
-
-    def calculate_payroll(self):
-        return self.hours_worked * self.hour_rate
-
-
-class CommissionEmployee(SalaryEmployee):
-
-    def __init__(self, id, name, weekly_salary, commission):
-        super().__init__(id, name, weekly_salary)
-        self.commission = commission
-
-    def calculate_payroll(self):
-        fixed = super().calculate_payroll()
-        return fixed + self.commission
 
 
 
